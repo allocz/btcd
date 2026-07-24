d9948c49beea8811d71cd3c5064e1be8c0c439ac
Olaoluwa Osuntokun <laolu32@gmail.com>
2026-07-24 16:04:20 -0700

    build: bump version to v0.26.2-beta
    In this commit, we move btcd past the v0.26.1 release candidate and set
    the development version to v0.26.2-beta. This keeps the daemon and RPC
    server version on the next patch series as work resumes on master.

--- version.go
+++ version.go
@@ -18,11 +18,11 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 0
 	appMinor uint = 26
-	appPatch uint = 1
+	appPatch uint = 2
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	appPreRelease = "beta.rc1"
+	appPreRelease = "beta"
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
