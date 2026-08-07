611a32d72b11ee006334ad409ff5fb54e216227c
allocz <alloczr@gmail.com>
2026-08-07 19:10:06 +0000

    rpctest: use option structs to create and setup harness
    The usage of option structs allows cleaner calls when the default
    behavior is needed and also allows future extensions without breaking
    the API.

--- integration/rpcserver_test.go
+++ integration/rpcserver_test.go
@@ -19,7 +19,6 @@ import (
 
 	"github.com/btcsuite/btcd/blockchain"
 	"github.com/btcsuite/btcd/btcjson"
-	"github.com/btcsuite/btcd/chaincfg/v2"
 	"github.com/btcsuite/btcd/chainhash/v2"
 	"github.com/btcsuite/btcd/integration/rpctest"
 	"github.com/btcsuite/btcd/rpcclient"
@@ -343,10 +342,9 @@ func TestMain(m *testing.M) {
 	// In order to properly test scenarios on as if we were on mainnet,
 	// ensure that non-standard transactions aren't accepted into the
 	// mempool or relayed.
-	btcdCfg := []string{"--rejectnonstd"}
-	primaryHarness, err = rpctest.New(
-		&chaincfg.SimNetParams, nil, btcdCfg, "",
-	)
+	primaryHarness, err = rpctest.New(&rpctest.HarnessOpts{
+		ExtraArgs: []string{"--rejectnonstd"},
+	})
 	if err != nil {
 		fmt.Println("unable to create primary harness: ", err)
 		os.Exit(1)
@@ -355,7 +353,10 @@ func TestMain(m *testing.M) {
 	// Initialize the primary mining node with a chain of length 125,
 	// providing 25 mature coinbases to allow spending from for testing
 	// purposes.
-	if err := primaryHarness.SetUp(true, 25); err != nil {
+	if err := primaryHarness.SetUp(&rpctest.SetUpOpts{
+		CreateTestChain:  true,
+		NumMatureOutputs: 25,
+	}); err != nil {
 		fmt.Println("unable to setup test chain: ", err)
 
 		// Even though the harness was not fully setup, it still needs
